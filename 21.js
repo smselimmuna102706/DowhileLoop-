4eb07f61863853fde7291816124a6011a072a274
smselimmuna102706 <111196494+smselimmuna102706@users.noreply.github.com>
2022-09-11 19:39:26 +0600

    DowhileLoop

---
+++ 21.js
@@ -0,0 +1,19 @@
+
+
+//while loop body
+
+var i = 1;
+while(i<=10){
+    document.write("</br>"+ i);
+    i++;
+}
+
+// do while body
+
+var i=11;
+
+do{
+    document.write("</br>" +i)
+    i++;
+}
+while(i<=20);
